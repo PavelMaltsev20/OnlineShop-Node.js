569e4e881d41cced5559a52d5d17ce96e6ab6296
Pavel <maltsevpavel95@gmail.com>
2021-10-01 19:08:37 +0300

    Create project, Add 3td libraries: express, nodemon. Create routes and views folder

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded());
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>404</h1><p>Page not found</p>`);
+});
+
+app.listen(3000);
